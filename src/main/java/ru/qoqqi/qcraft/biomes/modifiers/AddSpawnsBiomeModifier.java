b98103871c87a7f40075cba722340fcbf87d8976
Qoqqi <sazykin.georgii@gmail.com>
2023-04-05 13:50:03 +0300

    Рефакторинг AddSpawnsBiomeModifier

--- src/main/java/ru/qoqqi/qcraft/biomes/modifiers/AddSpawnsBiomeModifier.java
+++ src/main/java/ru/qoqqi/qcraft/biomes/modifiers/AddSpawnsBiomeModifier.java
@@ -1,9 +1,12 @@
 package ru.qoqqi.qcraft.biomes.modifiers;
 
+import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderSet;
+import net.minecraft.util.ExtraCodecs;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.MobSpawnSettings;
@@ -12,12 +15,33 @@ import net.minecraftforge.common.world.MobSpawnSettingsBuilder;
 import net.minecraftforge.common.world.ModifiableBiomeInfo;
 
 import java.util.List;
+import java.util.function.Function;
 
 public record AddSpawnsBiomeModifier(
 		MobCategory mobCategory,
 		HolderSet<Biome> biomes,
 		List<MobSpawnSettings.SpawnerData> spawners
 ) implements BiomeModifier {
+	
+	private static final Codec<List<MobSpawnSettings.SpawnerData>> SPAWNER_CODEC =
+			new ExtraCodecs.EitherCodec<>(
+					MobSpawnSettings.SpawnerData.CODEC.listOf(),
+					MobSpawnSettings.SpawnerData.CODEC
+			).xmap(
+					either -> either.map(Function.identity(), List::of),
+					list -> list.size() == 1
+							? Either.right(list.get(0))
+							: Either.left(list)
+			);
+	
+	public static final Codec<AddSpawnsBiomeModifier> CODEC = RecordCodecBuilder.create(builder -> builder.group(
+			MobCategory.CODEC.fieldOf("mob_category")
+					.forGetter(AddSpawnsBiomeModifier::mobCategory),
+			Biome.LIST_CODEC.fieldOf("biomes")
+					.forGetter(AddSpawnsBiomeModifier::biomes),
+			SPAWNER_CODEC.fieldOf("spawners")
+					.forGetter(AddSpawnsBiomeModifier::spawners)
+	).apply(builder, AddSpawnsBiomeModifier::new));
 	
 	@Override
 	public void modify(Holder<Biome> biome, Phase phase, ModifiableBiomeInfo.BiomeInfo.Builder builder) {
@@ -31,6 +55,6 @@ public record AddSpawnsBiomeModifier(
 	
 	@Override
 	public Codec<? extends BiomeModifier> codec() {
-		return ModBiomeModifierTypes.ADD_SPAWNS_BIOME_MODIFIER_TYPE.get();
+		return CODEC;
 	}
 }
--- src/main/java/ru/qoqqi/qcraft/biomes/modifiers/ModBiomeModifierTypes.java
+++ src/main/java/ru/qoqqi/qcraft/biomes/modifiers/ModBiomeModifierTypes.java
@@ -1,21 +1,11 @@
 package ru.qoqqi.qcraft.biomes.modifiers;
 
-import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
 
-import net.minecraft.util.ExtraCodecs;
-import net.minecraft.world.entity.MobCategory;
-import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.biome.MobSpawnSettings;
 import net.minecraftforge.common.world.BiomeModifier;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-
-import java.util.List;
-import java.util.function.Function;
 
 import ru.qoqqi.qcraft.QCraft;
 
@@ -24,18 +14,9 @@ public class ModBiomeModifierTypes {
 	public static final DeferredRegister<Codec<? extends BiomeModifier>> BIOME_MODIFIER_SERIALIZERS
 			= DeferredRegister.create(ForgeRegistries.Keys.BIOME_MODIFIER_SERIALIZERS, QCraft.MOD_ID);
 	
-	public static final RegistryObject<Codec<AddSpawnsBiomeModifier>> ADD_SPAWNS_BIOME_MODIFIER_TYPE =
-			BIOME_MODIFIER_SERIALIZERS.register(
-					"add_spawns",
-					() -> RecordCodecBuilder.create(builder -> builder.group(
-							MobCategory.CODEC.fieldOf("mob_category").forGetter(AddSpawnsBiomeModifier::mobCategory),
-							Biome.LIST_CODEC.fieldOf("biomes").forGetter(AddSpawnsBiomeModifier::biomes),
-							new ExtraCodecs.EitherCodec<>(MobSpawnSettings.SpawnerData.CODEC.listOf(), MobSpawnSettings.SpawnerData.CODEC).xmap(
-									either -> either.map(Function.identity(), List::of),
-									list -> list.size() == 1 ? Either.right(list.get(0)) : Either.left(list)
-							).fieldOf("spawners").forGetter(AddSpawnsBiomeModifier::spawners)
-					).apply(builder, AddSpawnsBiomeModifier::new))
-			);
+	static {
+		BIOME_MODIFIER_SERIALIZERS.register("add_spawns", () -> AddSpawnsBiomeModifier.CODEC);
+	}
 	
 	public static void register(IEventBus eventBus) {
 		BIOME_MODIFIER_SERIALIZERS.register(eventBus);
